698f74abaae63cd30f7103925d85735e020bfd2a
Hiruni Himasha <rp.hiruni.himasha@gmail.com>
2025-01-12 14:27:06 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [loading, setLoading] = useState(true); // Loading state
-  const url = "https://food-del-backend.netlify.app";
+  const url = "food-delivery-shcs.vercel.app";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
