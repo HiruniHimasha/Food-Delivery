cc072539421be6974bff00c3bb8be8569146548a
Hiruni Himasha <rp.hiruni.himasha@gmail.com>
2025-01-12 11:28:43 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,8 @@ import orderIcon from './order_icon.png'
 import uploadArea from './upload_area.png'
 import parcelIcon from './parcel_icon.png'
 
+export const url="https://capable-dragon-ab9695.netlify.app/"
+export const currency = "usd"
 export const assets ={
     logo,
     profile,
